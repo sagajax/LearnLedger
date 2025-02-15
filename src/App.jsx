f90b35687a16e63c6c52a9912c4944e050ff0e0d
sagajax <129790355+sagajax@users.noreply.github.com>
2025-02-15 21:09:28 +0530

    organized imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from 'react'
 import './App.css'
+import FeatureGrid from './components/FeatureGrid'
+import Footer from './components/Footer'
+import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
 import Pg from './components/Pg'
-import Footer from './components/Footer'
 import SetupOneKey from './components/SetupOneKey'
-import FeatureGrid from './components/FeatureGrid'
 import Slider from './components/Slider'
-import HeroSection from './components/HeroSection'
 
 
-import BlackWarpBackground from './components/BlackWrapBackground'
-import RootLayout from './RootLayout'
-import FAQSection from './components/FAQ'
-import PartnersSection from './components/Partner'
 import Banner from './components/Banner'
 import BentoGrid from './components/Bento'
-import Phone from './components/Phone'
+import BlackWarpBackground from './components/BlackWrapBackground'
+import FAQSection from './components/FAQ'
+import PartnersSection from './components/Partner'
+import RootLayout from './RootLayout'
 
 function App() {
   return (
